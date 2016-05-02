d3b2d1e81d99d138db9fb71a6751c73d7108428a
sblanc@redhat.com <sblanc@redhat.com>
2016-05-02 09:48:47 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,3 +1,17 @@
+var auth;
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+      var keycloakAuth = new Keycloak('keycloak.json');
+        keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+        auth = keycloakAuth;
+
+    }).error(function () {
+          //  window.location.reload();
+        });
+}
+
+
 document.getElementById('say_hello').onclick = function () {
   document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
   $fh.cloud(
